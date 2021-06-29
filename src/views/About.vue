5c0653b5c1eb9b494588c28f5338e78c9d3dfc7b
Eyes22798 <eyes22798@gmail.com>
2021-06-29 22:05:59 +0800

    feat: add html

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="header">
+      <input type="text">
+    </div>
+    <div class="main">
+      <ul>
+        <li>2112</li>
+      </ul>
+    </div>
+    <div class="footer">
+      <div class="info">info</div>
+      <div class="button">
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
   </div>
 </template>
